96bead473f969223fa4c11729994c94e97bd6049
Sravanam charan <sravanamcharan20@gmail.com>
2024-03-25 16:29:49 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'upload.wikimedia.org',
-          pathname: '**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
