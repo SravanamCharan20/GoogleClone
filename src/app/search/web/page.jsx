f88d87692bf6dc784c324e454c79f8241e21d503
Sravanam charan <sravanamcharan20@gmail.com>
2024-03-25 14:59:52 +0530

    added page.js functionality

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_TEXT}&q=${searchParams.searchTerm}`);
+  const data = await response.json();
+  const results = data.items;
 
-export default function WebSearchPage() {
   return (
     <div>
-      websearchpage
+      {results && results.map((result, index) => (
+        <h1 key={index}>{result.title}</h1>
+      ))}
     </div>
-  )
+  );
 }
