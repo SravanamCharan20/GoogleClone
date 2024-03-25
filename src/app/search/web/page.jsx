6d27c3f02c2cb738651fed41e194bec7122458cd
Sravanam charan <sravanamcharan20@gmail.com>
2024-03-25 16:15:10 +0530

    loading effects added

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 export default async function WebSearchPage({ searchParams }) {
   const startIndex = searchParams.start || '1';
+  await new Promise((resolve)=>setTimeout(resolve,1000));
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_TEXT}&q=${searchParams.searchTerm}&start=${startIndex}`);
   
   if (!response.ok) throw new Error("Something went wrong");
