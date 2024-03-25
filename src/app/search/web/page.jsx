b152d12803fa29ae1b4bac3d9cacbe3671cc5d30
Sravanam charan <sravanamcharan20@gmail.com>
2024-03-25 15:19:16 +0530

    handled possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,12 +1,34 @@
+import Link from "next/link";
+
 export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_TEXT}&q=${searchParams.searchTerm}`);
+  
+  if (!response.ok) throw new Error("Something went wrong");
+
   const data = await response.json();
   const results = data.items;
 
+  if (!results || results.length === 0) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching the web or images for something else
+          <Link href='/'>
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
   return (
     <div>
-      {results && results.map((result, index) => (
-        <h1 key={index}>{result.title}</h1>
+      {results.map((result, index) => (
+        <div key={index}>
+          <h1>{result.title}</h1>
+        </div>
       ))}
     </div>
   );
