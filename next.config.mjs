3922561d02cfddd896b86437fc5bddd7a8288a12
Sravanam charan <sravanamcharan20@gmail.com>
2024-03-25 13:32:08 +0530

    homesearch and it functionalities

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'upload.wikimedia.org',
+          pathname: '**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
-import HomeHeader from '@/components/HomeHeader'
-import React from 'react'
+'use client'
+import HomeHeader from '@/components/HomeHeader';
+import HomeSearch from '@/components/HomeSearch';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  const handleSearch = (searchTerm) => {
+    router.push(`/search/web?searchTerm=${searchTerm}`);
+  };
   return (
     <>
-      <HomeHeader/>
+      <HomeHeader />
+      <div className='flex flex-col items-center mt-24'>
+        <Image
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
+          alt='Google Logo'
+          width={300}
+          height={100}
+          priority
+          style={{ width: 'auto' }}
+        />
+        <HomeSearch onSearch={handleSearch}></HomeSearch>
+      </div>
     </>
-  )
+  );
 }
